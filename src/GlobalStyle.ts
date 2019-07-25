336a0d10c969217151052e8c1d5d3427b04b26ae
박기완 <zprime0920@gmail.com>
2019-07-25 22:14:23 +0900

    Feature: 기본적인 스타일 적용

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -5,9 +8,11 @@ body {
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
 
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
+  * {
+    box-sizing: border-box;
+  }
 }
+`;
+
+export default GlobalStyle;
