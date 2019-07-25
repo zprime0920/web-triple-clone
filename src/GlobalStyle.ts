19798057ab5e9ea60a75dc21c31b43fa9adc5646
박기완 <zprime0920@gmail.com>
2019-07-25 22:28:13 +0900

    Fix: 들여쓰기 누락 수정

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,18 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 
-  * {
-    box-sizing: border-box;
+    * {
+      box-sizing: border-box;
+    }
   }
-}
 `;
 
 export default GlobalStyle;
