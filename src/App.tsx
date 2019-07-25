1d42f2924e4b163ffde4f66c261a13eaa1c61e27
박기완 <zprime0920@gmail.com>
2019-07-25 21:42:32 +0900

    Feature: 기본적인 구조 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import imgTriple from 'assets/triple@2x.png';
+import imgPlayStore from 'assets/play-store@2x.png';
+import imgAppStore from 'assets/app-store@2x.png';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <img
+          src={imgTriple}
+          alt="트리플 이미지"
+        />
+        2019년 5월 기준
+      </div>
+      <div>
+        <div>
+          <div>410만 명의 여행자</div>
+          <div>40만 개의 여행 리뷰</div>
+          <div>250만 개의 여행 일정</div>
+        </div>
+        <div>
+          <div>
+            <img src={imgPlayStore} alt="플레이스토어 올해의 앱 최우수상 이미지" />
+            <div>
+              2018 구글 플레이스토어<br/>
+              올해의 앱 최우수상 수상
+            </div>
+          </div>
+          <div>
+            <img src={imgAppStore} alt="앱스토어 이미지" />
+            <div>
+              2018 애플 앱스토어<br/>
+              오늘의 여행앱 선정
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
